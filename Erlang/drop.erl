48ee9842f8be163e5a8852fd743f5a155642f1c7
xDOASx <benesbit@yahoo.com>
2019-05-14 11:24:36 -0700

    Encapsulate arguements into a tuple

--- Erlang/drop.erl
+++ Erlang/drop.erl
@@ -5,10 +5,10 @@
 %% Introducing Erlang</a>
 
 -module(drop).
--export([fall_velocity/2]).
+-export([fall_velocity/1]).
 
-fall_velocity(earth, Distance) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
+fall_velocity({earth, Distance}) when Distance >= 0 -> math:sqrt(2 * 9.8 * Distance);
 
-fall_velocity(moon, Distance) when Distance -> math:sqrt(2 * 1.6 * Distance);
+fall_velocity({moon, Distance}) when Distance -> math:sqrt(2 * 1.6 * Distance);
 
-fall_velocity(mars, Distance) when Distance -> math:sqrt(2 * 3.71 * Distance).
+fall_velocity({mars, Distance}) when Distance -> math:sqrt(2 * 3.71 * Distance).
