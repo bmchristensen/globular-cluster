4017f9927a96c098575dd206b5181253e6c86550
xDOASx <benesbit@yahoo.com>
2019-05-14 12:22:43 -0700

    Refactor drop to use return of case, cleaning up code.

--- Erlang/drop.erl
+++ Erlang/drop.erl
@@ -8,10 +8,10 @@
 -export([fall_velocity/2]).
 
 fall_velocity(Planemo, Distance) when Distance >= 0 ->
-  case Planemo of
-    earth -> math:sqrt(2 * 9.8 * Distance);
-    moon -> math:sqrt(2 * 1.6 * Distance);
-    mars -> math:sqrt(2 * 3.71 * Distance);
-    _ -> "unknown planet" % This should always be the last case, nothing after
-                          % it gets evaluated.
-  end.
+  Gravity = case Planemo of
+    earth -> 9.8;
+    moon -> 1.6;
+    mars -> 3.71
+  end,      % comma, function ain't over yet boyo
+
+  math:sqrt(2 * Gravity * Distance).
