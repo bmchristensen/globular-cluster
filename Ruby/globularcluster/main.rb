03a6e5f927c2ebc217061005149bfa554a5b34d1
bmchristensen <bmchristensen@gmail.com>
2019-05-15 15:54:14 -0700

    Initial commit for Ruby app implementation
    Create a basic game template in main with simple key capture controls. A ship class provides a method to print to standard out. Run 'ruby main.rb'.

---
+++ Ruby/globularcluster/main.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require_relative 'ship'
+
+class GlobularCluster
+  attr_accessor :ship
+
+  def initialize
+    @ship = Ship.new
+  end
+
+  def draw
+    ship.draw
+  end
+
+  def show
+    loop do
+      # update
+      draw
+      key_pressed(gets.chomp)
+    end
+  end
+
+  def key_pressed(key)
+    if key == 'a'
+      ship.take_direct_route
+    elsif key == 'd'
+      ship.take_circuitous_route
+    elsif key == ' '
+      ship.attack
+    end
+  end
+end
+
+GlobularCluster.new.show
---
+++ Ruby/globularcluster/ship.rb
@@ -0,0 +1,9 @@
+class Ship
+  def draw
+    puts self
+  end
+
+  def to_s
+    '✈️'
+  end
+end
