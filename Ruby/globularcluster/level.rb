2ec3e7d8c9d56adbe4bffef2f807f054834ee592
bmchristensen <bmchristensen@gmail.com>
2019-05-17 13:38:28 -0700

    Add hash for level attributes

--- Ruby/globularcluster/level.rb
+++ Ruby/globularcluster/level.rb
@@ -1,9 +1,22 @@
 class Level
+  attr_accessor :level, :ship_location
+
+  def initialize
+    @level = {
+      'first' => ['👾', '🌑', 5],
+      'second' => ['alien: True', 'fuel: 0'],
+      'third' => ['alien: False', 'fuel: 0'],
+      'fourth' => ['alien: True', 'fuel: 1'],
+      'end' => ['alien: False', 'fuel: 0']
+    }
+  end
+
   def draw
     puts self
   end
 
   def to_s
-    "✈️ --> 👾 🌑  --> 👾️ 🌑️  --> 🌑️  --> 🌑️ 👾  --> 'end'"
+    # puts(level['first'][0])
+    "✈️ --> #{level['ship_location'][0]} #{level['ship_location'][1]} --> 👾️ 🌑️  --> 🌑️  --> 🌑️ 👾  --> 'end'"
   end
 end
