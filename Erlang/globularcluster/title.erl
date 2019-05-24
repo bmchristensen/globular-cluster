729912810f6704aa3a29e3f2e48551da875c17e9
bmchristensen <bmchristensen@gmail.com>
2019-05-24 11:09:44 -0700

    Read title characters from file

--- Erlang/globularcluster/title.erl
+++ Erlang/globularcluster/title.erl
@@ -1,5 +1,16 @@
 -module(title).
--compile(export_all).
+-export([show/0]).
 
 show() ->
-    io:format("GLOBULAR CLUSTER~n").
+    print_file("title_sequence.txt").
+
+print_file(Name) ->
+    {ok, Device} = file:open(Name, [read]),
+    get_lines(Device).
+
+get_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof  -> file:close(Device);
+        Line -> io:fwrite("~s", [Line]),
+                    get_lines(Device)
+    end.
