bb0d0358895239914dd1a29597433e7d49b3b22c
xDOASx <benesbit@yahoo.com>
2019-06-15 21:39:26 -0700

    Test: ship velocity.
    Test a ship\'s initial velocity, using default and desigated values.

--- Ruby/globularcluster/tests.rb
+++ Ruby/globularcluster/tests.rb
@@ -10,6 +10,8 @@ class ShipTest < Minitest::Test
 
   def test_a_ships_initial_velocity
     test_ship = Ship.new
+    test_ship_2 = Ship.new(:velocity => 4.20)
     assert test_ship.velocity == 11.53
+    assert test_ship_2.velocity == 4.20
   end
 end
