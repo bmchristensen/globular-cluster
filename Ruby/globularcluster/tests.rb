75d50e4a42156ebc5c229167561d086a8bd83d45
xDOASx <benesbit@yahoo.com>
2019-06-15 22:08:20 -0700

    Test: Test initial state of ship.
    Add test for ship initial fuel with no input.
    Refactor velocity test into two seperate unit tests.

--- Ruby/globularcluster/tests.rb
+++ Ruby/globularcluster/tests.rb
@@ -8,10 +8,18 @@ class ShipTest < Minitest::Test
     assert test_ship != nil
   end
 
-  def test_a_ships_initial_velocity
+  def test_a_ships_initial_velocity_with_no_input
     test_ship = Ship.new
-    test_ship_2 = Ship.new(:velocity => 4.20)
     assert test_ship.velocity == 11.53
-    assert test_ship_2.velocity == 4.20
+  end
+
+  def test_a_ships_initial_velocity_with_input
+    test_ship = Ship.new(:velocity => 4.20)
+    assert test_ship.velocity == 4.20
+  end
+
+  def test_a_ships_initial_fuel_with_no_input
+    test_ship = Ship.new
+    assert test_ship.fuel == 15
   end
 end
